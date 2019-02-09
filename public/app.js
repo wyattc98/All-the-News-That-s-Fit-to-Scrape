c638a836c6806dcc47ddaa36d00e1877b21d9405
Wyatt C <cochranwyatt@yahoo.com>
2019-02-09 15:46:04 -0800

    Updated JS and HTML Scrape still not working.

---
+++ public/app.js
@@ -0,0 +1,45 @@
+$.getJSON("/Article", function(data){
+    for (var i = 0; i < data.length; i ++){
+        $("#Article").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+});
+
+$(document).on("click", "p", function(){
+    $("#notes").empty();
+    var thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "GET",
+        url: "/Article/" + thisId
+    })
+
+    .then(function(data) {
+        $("#notes").append("<h2>" + data.title + "</h2>");
+        $("#notes").append("<input id='titleinput' name='title' >");
+        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+        $("#notes").append("<button data-id='" + data_id + "' id='savenote'>Save Note</button>");
+        
+        if (data.note) {
+            $("#titleinput").val(data.note.title);
+            $("#bodyinput").val(data.note.body);
+        }
+    });
+});
+
+$(document).on("click", "#savenote", function(){
+    var thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "POST",
+        url: "/" + thisId,
+        data: {
+            title: $("#titleinput").val(),
+            body: $("#bodyinput").val()
+        }
+    })
+    .then(function(data){
+        $("#notes").empty()
+    });
+    $("#titleinput").val("");
+    $("#bodyinput").val("")
+});
--- server.js
+++ server.js
@@ -5,16 +5,64 @@ const cheerio = require("cheerio");
 const logger = require("morgan")
 const db = require("./models")
 
-const PORT = 3000;
+const PORT = 8080;
 
 const app = express();
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hwscraperdb";
+
+mongoose.connect(MONGODB_URI);
+
+
 
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/hwscraperdb", { useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/hwscraperdb", { useNewUrlParser: true });
 
 //ADD Routes
+app.get("/", function (req, res) {
+
+
+    axios.get("https://www.nytimes.com/section/world").then(function (response) {
+        var $ = cheerio.load(response.data)
+
+        $("article h2").each(function(i, element) {
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+
+            result.link = $(this)
+                .children("a")
+                .attr("href")
+
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle)
+                })
+                .catch(function(err) {
+                    console.log(err)
+                })
+
+        });
+        res.send("Scrape Complete!")
+    });
+});
+
+app.get("/Article", function(){
+    db.Article.find({})
+    .then(function (dbArticle) {
+        res.json(dbArticle)
+    })
+    .catch(function (err) {
+        res.json(err)
+    });
+})
+
+app.listen(PORT, function () {
+    console.log("Listening on port: " + PORT + "!")
+});
